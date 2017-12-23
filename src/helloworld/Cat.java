7d52469c29619906160cb255ba6c61a57f044893
Ruchika2 <ruchikadube2@gmail.com>
2017-12-23 03:03:17 -0800

    Interface prog

--- src/helloworld/Cat.java
+++ src/helloworld/Cat.java
@@ -1,23 +1,7 @@
 package helloworld;
 
-public class Cat {
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		Toy obj = new Demo();
-		obj.getcolour();
-		Toy obj1=new Demo();
-		obj1.makesound();
-	}
-
-}
-  interface Toy{
-	   public void getcolour();
-	   public void makesound();
-  }
-  
-  
-  class Demo implements Toy{
-	  public void getcolour()
+public class Cat extends animal implements Toy{
+	 public void getcolour()
 	  {
 		  System.out.println("my colour is black");
 		  
@@ -27,5 +11,23 @@ public class Cat {
 		  System.out.println("Wolf Wolf");
 		  
 	  }
-	  
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Toy obj = new Cat();
+		obj.getcolour();
+		animal obj1=new Cat();
+		obj1.makesound();
+	}
+
+}
+  interface Toy{
+	   public void getcolour();
+	   
   }
+    abstract  class animal{
+	  abstract public void makesound();
+	 
+   }
+  
+  
+  
